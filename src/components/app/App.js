d1d8aab75092f8372e6f40ec44159e46c9174b47
Ruslan <97601970+russelrudoi@users.noreply.github.com>
2023-03-05 13:56:11 +0200

    Fix gh-pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import ScrollToTop from '../../utils/scrollToTop';
